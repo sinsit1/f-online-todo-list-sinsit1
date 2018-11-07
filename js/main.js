818e04ae705bde10c0ab83ec0ab6383ff2c99099
sinsit1 <alexandra8989@gmail.com>
2018-11-07 13:40:28 +0100

    add logic. add task

--- js/main.js
+++ js/main.js
@@ -2,14 +2,35 @@
 
 const addTaskButton =  document.getElementById('add__task');
 const date = document.getElementById('date');
+const modal = document.getElementById('modal');
+const taskInputValue = document.getElementById('task_value');
+const itemsList = document.getElementById('itemsList');
 
-function addHandler() {
-   
+
+function addTask(param) {
+    
+    const taskcontainer = document.createElement('li');
+    const falseCheckBox = document.createElement('div');
+    const valueParraf = document.createElement('p');
+    falseCheckBox .type = 'checkbox';
+    valueParraf.innerHTML= param;
+
+    itemsList.appendChild(taskcontainer);
+    taskcontainer.appendChild(falseCheckBox);
+    taskcontainer.appendChild(valueParraf);
+    taskInputValue.value='';
 }
 
-function addTask() {
-    console.log('add task');
+function showModal(){
+    modal.classList.toggle('hidden');
+}
 
+function saveTask() {
+    let task_value = this.value;
+    console.log('value',this.value);
+    modal.classList.toggle('hidden');
+
+    addTask(task_value);
 }
 
 function getDate(){
@@ -20,5 +41,6 @@ function getDate(){
     document.write('<p class="date" id="date">'+ diasSemana[f.getDay()] + ", " + f.getDate() + meses[f.getMonth()] +  f.getFullYear()+'</p>');
 }
 
-addTaskButton.addEventListener('click', addTask);
+addTaskButton.addEventListener('click', showModal);
+taskInputValue.addEventListener('change', saveTask);
 getDate();
